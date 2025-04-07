43eca81f8d655908890dc7f1c4cbdb79ce77918d
shashank <ishisenku15@gmail.com>
2025-04-07 11:51:39 +0530

    cors

--- server.js
+++ server.js
@@ -5,12 +5,24 @@ const cors=require("cors")
 const server=http.createServer(app)
 const controller=require("./controller")
 app.use(express.json())
+
+const allowedOrigins = [
+  "http://127.0.0.1:5500",
+  ];
+
 app.use(
   cors({
-    origin: "*",
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true); // Allow the request
+      } else {
+        callback(new Error("Not allowed by CORS")); // Deny the request
+      }
+    },
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
   })
 );
-
 
 app.post("/submit",controller.form)
 app.get("/msg",controller.msg)
